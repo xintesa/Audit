01a8cf14842fd2760a7dc767b4d3b6749471f670
Rachman Chavik <rchavik@xintesa.com>
2013-09-09 13:37:44 +0700

    Changing spaces to tabs

--- Test/Fixture/AuthorFixture.php
+++ Test/Fixture/AuthorFixture.php
@@ -1,21 +1,23 @@
 <?php
 
 class AuthorFixture extends CakeTestFixture {
-  public $name = 'Author';
 
-  public $fields = array(
-    'id'         => array('type' => 'integer', 'key' => 'primary'),
-    'first_name' => array('type' => 'string', 'null' => false),
-    'last_name'  => array('type' => 'string', 'null' => false),
-    'created'    => 'datetime',
-    'updated'    => 'datetime'
-  );
+	public $name = 'Author';
 
-  /**
-   * records property
-   *
-   * @public array
-   * @access public
-   */
-  public $records = array();
+	public $fields = array(
+		'id'         => array('type' => 'integer', 'key' => 'primary'),
+		'first_name' => array('type' => 'string', 'null' => false),
+		'last_name'  => array('type' => 'string', 'null' => false),
+		'created'    => 'datetime',
+		'updated'    => 'datetime'
+	);
+
+/**
+ * records property
+ *
+ * @public array
+ * @access public
+ */
+	public $records = array();
+
 }
